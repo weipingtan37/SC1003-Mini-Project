68463a157ff26427c9463b2ee27c0189da8220e3
Weipingtan37 <weipingtan37@gmail.com>
2024-10-19 17:12:37 +0800

    read file and split cgpa groups

---
+++ FCS2_Group_5.py
@@ -0,0 +1,24 @@
+# Initialize Global Variable STUDENTS_LIST and append the information from the CSV File 
+STUDENTS_LIST = []
+with open('records.csv', 'r') as records:
+    next(records) # Start from second line onwards
+    for student in records:
+        tutorial_group, student_id, name, school, gender, cgpa = student.strip().split(',')
+        STUDENTS_LIST.append({
+            'Tutorial Group': tutorial_group,
+            'Student ID': student_id,
+            'Name': name,
+            'School': school,
+            'Gender': gender,
+            'CGPA': float(cgpa)
+        })
+
+        def cgpa_groups(cgpa):
+            if cgpa >= 4.5:
+                return 'High'
+            elif cgpa >= 4.0:
+                return 'Mid'
+            else:
+                return 'Low'
+        for student in STUDENTS_LIST:
+            student['CGPA Group'] = cgpa_groups(student['CGPA'])
